ce8d13222b19f345b925694c0d16453ed7985c5a
HarounMehdaoui <mehdaoui.haroun.edu@gmail.com>
2025-10-24 15:38:57 +0100

    chore: move firebase config to env vars, add .env.local.example and .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
+// Configuration now read from environment variables.
+// For Create React App, env vars must be prefixed with REACT_APP_
 const firebaseConfig = {
-    apiKey: "AIzaSyCr89tryLCDib-KOC4e6aleWni8iBdxfTo",
-    authDomain: "chat-application-8ff7d.firebaseapp.com",
-    projectId: "chat-application-8ff7d",
-    storageBucket: "chat-application-8ff7d.appspot.com",
-    messagingSenderId: "1000522788422",
-    appId: "1:1000522788422:web:6db8ac69aaf60d5439e447",
-    measurementId: "G-P0CQLB2403"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+// Basic runtime check to help catch missing env values in development.
+if (!firebaseConfig.apiKey) {
+    // eslint-disable-next-line no-console
+    console.warn(
+        "Firebase API key is not set. Create a .env.local with the required REACT_APP_FIREBASE_* variables."
+    );
+}
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
