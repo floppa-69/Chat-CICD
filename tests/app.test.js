2d32eb017f15addd4dfdf2b7431cc2c12ddfb664
floppa-69 <yassine.benromathan@icloud.com>
2025-10-26 23:16:53 +0100

    Updated tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,3 +1,11 @@
-test('Basic math test', () => {
-  expect(2 + 2).toBe(4);
+import { render } from '@testing-library/react';
+import App from './App';
+import { UserProvider } from './context/UserContext';
+
+test('App renders without crashing', () => {
+  render(
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
 });
